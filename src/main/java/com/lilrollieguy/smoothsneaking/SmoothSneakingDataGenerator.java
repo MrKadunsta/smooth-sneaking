68a53c3163e59c4b6daf7d6ac139fd5207e018f4
Thomas Eberle <lilrollieguy@icloud.com>
2025-08-04 19:08:47 -0500

    Initial commit

---
+++ src/main/java/com/lilrollieguy/smoothsneaking/SmoothSneakingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lilrollieguy.smoothsneaking;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SmoothSneakingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
